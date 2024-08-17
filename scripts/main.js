0b3238f541eb7cccff1e62636ec1e86ef8bcc83f
theguy951357 <crispspiceguitar@gmail.com>
2024-08-17 15:09:14 -0400

    added colors

--- scripts/main.js
+++ scripts/main.js
@@ -8,15 +8,15 @@ function onClick(element) {
 }
 
 // Change style of navbar on scroll
-window.onscroll = function() {myFunction()};
-function myFunction() {
-    var navbar = document.getElementById("myNavbar");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " w3-white";
-    } else {
-        navbar.className = navbar.className.replace(" w3-card w3-animate-top w3-white", "");
-    }
-}
+// window.onscroll = function() {myFunction()};
+// function myFunction() {
+//     var navbar = document.getElementById("myNavbar");
+//     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+//         navbar.className = "w3-bar" + " w3-card" + " w3-animate-top" + " greenbackground";
+//     } else {
+//         navbar.className = navbar.className.replace(" w3-card w3-animate-top greenbackground", "");
+//     }
+// }
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
